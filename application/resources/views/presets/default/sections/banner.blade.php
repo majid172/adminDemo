14935a4d4de127660eb99785101cada5b73849ad
majid172 <luthfullahhilmajid172@gmail.com>
2024-05-05 18:10:33 +0600

    banner

---
+++ application/resources/views/presets/default/sections/banner.blade.php
@@ -0,0 +1,34 @@
+@php
+    $content = getContent('banner.content',true);
+@endphp
+<section class="mt-8">
+    <div class="container">
+        <div class="hero-slider">
+            <div style="background: url(assets/images/slider/slide-1.jpg) no-repeat; background-size: cover; border-radius: 0.5rem; background-position: center">
+                <div class="ps-lg-12 py-lg-16 col-xxl-5 col-md-7 py-14 px-8 text-xs-center">
+                    <span class="badge text-bg-warning">{{__($content->data_values->subheading_1)}}</span>
+
+                    <h2 class="text-dark display-5 fw-bold mt-4">{{__($content->data_values->heading_1)}}</h2>
+                    <p class="lead">{{__($content->data_values->paragraph_1)}}</p>
+                    <a href="#" class="btn btn-dark mt-3">
+                        Shop Now
+                        <i class="feather-icon icon-arrow-right ms-1"></i>
+                    </a>
+                </div>
+            </div>
+            <div style="background: url(assets/images/slider/slider-2.jpg) no-repeat; background-size: cover; border-radius: 0.5rem; background-position: center">
+                <div class="ps-lg-12 py-lg-16 col-xxl-5 col-md-7 py-14 px-8 text-xs-center">
+                    <span class="badge text-bg-warning">{{__($content->data_values->subheading_2)}}</span>
+                    <h2 class="text-dark display-5 fw-bold mt-4">
+                        {{__($content->data_values->heading_2)}}
+                    </h2>
+                    <p class="lead">{{__($content->data_values->paragraph_2)}}</p>
+                    <a href="#" class="btn btn-dark mt-3">
+                        Shop Now
+                        <i class="feather-icon icon-arrow-right ms-1"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
