66eafbf23765c9bc58dba68eb4407ddc70992667
majid172 <luthfullahhilmajid172@gmail.com>
2024-05-09 13:33:34 +0600

    product, order, orderItem table

---
+++ application/database/migrations/2024_05_09_060033_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->integer('cat_id');
+            $table->string('name',40);
+            $table->decimal('price',28,8);
+            $table->integer('discount');
+            $table->integer('quantity');
+            $table->longText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
