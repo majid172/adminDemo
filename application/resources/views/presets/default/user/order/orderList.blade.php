72efafbff1cbcb2e0e8029aa21d6814b5912a6b8
majid172 <luthfullahhilmajid172@gmail.com>
2024-06-13 15:17:31 +0600

    sidbar dynamically active

--- application/resources/views/presets/default/user/order/orderList.blade.php
+++ application/resources/views/presets/default/user/order/orderList.blade.php
@@ -22,60 +22,8 @@
                         </button>
                     </div>
                 </div>
-                <!-- col -->
-                <div class="col-lg-3 col-md-4 col-12 border-end d-none d-md-block">
-                    <div class="pt-10 pe-lg-10">
-                        <!-- nav -->
-                        <ul class="nav flex-column nav-pills nav-pills-dark">
-                            <!-- nav item -->
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="{{route('user.orders.list')}}">
-                                    <i class="feather-icon icon-shopping-bag me-2"></i>
-                                    @lang('My Orders')
-                                </a>
-                            </li>
-                            <!-- nav item -->
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{route('user.profile.setting')}}">
-                                    <i class="feather-icon icon-settings me-2"></i>
-                                    @lang('Settings')
-                                </a>
-                            </li>
-                            <!-- nav item -->
-                            <li class="nav-item">
-                                <a class="nav-link" href="account-address.html">
-                                    <i class="feather-icon icon-map-pin me-2"></i>
-                                    Address
-                                </a>
-                            </li>
-                            <!-- nav item -->
-                            <li class="nav-item">
-                                <a class="nav-link" href="account-payment-method.html">
-                                    <i class="feather-icon icon-credit-card me-2"></i>
-                                    Payment Method
-                                </a>
-                            </li>
-                            <!-- nav item -->
-                            <li class="nav-item">
-                                <a class="nav-link" href="account-notification.html">
-                                    <i class="feather-icon icon-bell me-2"></i>
-                                    Notification
-                                </a>
-                            </li>
-                            <!-- nav item -->
-                            <li class="nav-item">
-                                <hr />
-                            </li>
-                            <!-- nav item -->
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{route('user.logout')}}">
-                                    <i class="feather-icon icon-log-out me-2"></i>
-                                    @lang('Log out')
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+
+                @include($activeTemplate.'user.order.sidebar')
                 <div class="col-lg-9 col-md-8 col-12">
                     <div class="py-6 p-md-6 p-lg-10">
                         <!-- heading -->
---
+++ application/resources/views/presets/default/user/order/sidebar.blade.php
@@ -0,0 +1,54 @@
+<div class="col-lg-3 col-md-4 col-12 border-end d-none d-md-block">
+    <div class="pt-10 pe-lg-10">
+        <!-- nav -->
+        <ul class="nav flex-column nav-pills nav-pills-dark">
+            <!-- nav item -->
+            <li class="nav-item">
+                <a class="nav-link {{ request()->routeIs('user.orders.list') ? 'active' : '' }}" aria-current="page" href="{{route('user.orders.list')}}">
+                    <i class="feather-icon icon-shopping-bag me-2"></i>
+                    @lang('My Orders')
+                </a>
+            </li>
+            <!-- nav item -->
+            <li class="nav-item">
+                <a class="nav-link {{ request()->routeIs('user.profile.setting') ? 'active' : '' }}" href="{{route('user.profile.setting')}}">
+                    <i class="feather-icon icon-settings me-2"></i>
+                    @lang('Settings')
+                </a>
+            </li>
+            <!-- nav item -->
+            <li class="nav-item">
+                <a class="nav-link {{ request()->routeIs('user.orders.list') ? 'active' : '' }}" href="account-address.html">
+                    <i class="feather-icon icon-map-pin me-2"></i>
+                    Address
+                </a>
+            </li>
+            <!-- nav item -->
+            <li class="nav-item">
+                <a class="nav-link" href="account-payment-method.html">
+                    <i class="feather-icon icon-credit-card me-2"></i>
+                    Payment Method
+                </a>
+            </li>
+            <!-- nav item -->
+            <li class="nav-item">
+                <a class="nav-link" href="account-notification.html">
+                    <i class="feather-icon icon-bell me-2"></i>
+                    Notification
+                </a>
+            </li>
+            <!-- nav item -->
+            <li class="nav-item">
+                <hr />
+            </li>
+            <!-- nav item -->
+            <li class="nav-item">
+                <a class="nav-link {{ request()->routeIs('user.logout') ? 'active' : '' }}" href="{{route('user.logout')
+                }}">
+                    <i class="feather-icon icon-log-out me-2"></i>
+                    @lang('Log out')
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
