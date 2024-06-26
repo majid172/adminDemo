f1fe2a55b6781db7f519ea6c46f0c0d73e4795ef
majid172 <luthfullahhilmajid172@gmail.com>
2024-06-26 11:02:32 +0600

    login

--- application/resources/views/presets/default/user/auth/login.blade.php
+++ application/resources/views/presets/default/user/auth/login.blade.php
@@ -23,7 +23,7 @@
                 <div class="row justify-content-center align-items-center">
                     <div class="col-12 col-md-6 col-lg-4 order-lg-1 order-2">
                         <!-- img -->
-                        <img src="{{asset('assets/images/signin.png')}}" alt="" class="img-fluid" />
+                        <img src="{{asset('assets/images/signin.png')}}" alt="signin" class="img-fluid" />
                     </div>
                     <!-- col -->
                     <div class="col-12 col-md-6 offset-lg-1 col-lg-4 order-lg-2 order-1">
@@ -32,21 +32,25 @@
                             <p>@lang('Welcome back to'.' '. $general->site_name.' '. 'Enter your email to get started.')</p>
                         </div>
 
-                        <form class="needs-validation" novalidate>
+{{--                        <form class="needs-validation" novalidate>--}}
+                        <form method="POST" action="{{ route('user.login') }}" class="verify-gcaptcha">
+                            @csrf
                             <div class="row g-3">
                                 <div class="col-12">
                                     <label for="formSigninEmail" class="form-label visually-hidden">@lang('Username')</label>
-                                    <input type="text" class="form-control" name="username" id="formSigninEmail" placeholder="Enter Username" required />
+                                    <input type="text" class="form-control" name="username" id="formSigninEmail" value="{{ old('username') }}"
+                                           placeholder="Enter Username" required />
                                     <div class="invalid-feedback">@lang('Please enter username.')</div>
                                 </div>
                                 <div class="col-12">
                                     <!-- input -->
                                     <div class="password-field position-relative">
-                                        <label for="formSigninPassword" class="form-label visually-hidden">Password</label>
+                                        <label for="formSigninPassword" class="form-label visually-hidden">@lang('Password')</label>
                                         <div class="password-field position-relative">
-                                            <input type="password" class="form-control fakePassword" id="formSigninPassword" placeholder="*****" required />
+                                            <input type="password" name="password" class="form-control fakePassword"
+                                                   id="formSigninPassword" placeholder="*****" required />
                                             <span><i class="bi bi-eye-slash passwordToggler"></i></span>
-                                            <div class="invalid-feedback">Please enter password.</div>
+                                            <div class="invalid-feedback">@lang('Please enter password.')</div>
                                         </div>
                                     </div>
                                 </div>
@@ -55,15 +59,17 @@
                                     <div class="form-check">
                                         <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
                                         <!-- label -->
-                                        <label class="form-check-label" for="flexCheckDefault">Remember me</label>
+                                        <label class="form-check-label" for="flexCheckDefault">@lang('Remember me')
+                                        </label>
                                     </div>
                                     <div>
-                                        Forgot password?
-                                        <a href="{{ route('user.password.request') }}">Reset It</a>
+                                        @lang('Forgot password?')
+                                        <a href="{{ route('user.password.request') }}">@lang('Reset It')</a>
                                     </div>
                                 </div>
                                 <!-- btn -->
-                                <div class="col-12 d-grid"><button type="submit" class="btn btn-primary">Sign In</button></div>
+                                <div class="col-12 d-grid"><button type="submit" class="btn btn-primary">@lang('Sign In')
+                                    </button></div>
                                 <!-- link -->
                                 <div>
                                     @lang('Don\’t have an account?')
