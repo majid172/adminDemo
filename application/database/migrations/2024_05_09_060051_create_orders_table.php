bb5394b1b468dd6210e778fde7e36aa5d93720b5
majid172 <luthfullahhilmajid172@gmail.com>
2024-06-29 12:37:58 +0600

    add total_amount and shipping address col in order table

--- application/database/migrations/2024_05_09_060051_create_orders_table.php
+++ application/database/migrations/2024_05_09_060051_create_orders_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
             $table->tinyInteger('status')->default(0);
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
 
---
+++ application/database/migrations/2024_06_29_063246_add_total_amount_and_shipping_address_to_orders_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->decimal('total_amount',28,8)->after('user_id');
+            $table->text('shipping_address')->after('total_amount');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn('total_amount');
+            $table->dropColumn('shipping_address');
+        });
+    }
+};
